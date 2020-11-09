e047dfffebd20675bcc12634f6d1fc144e488c17
leeha0 <gkdud2837@gmail.com>
2020-11-09 21:58:06 +0900

    [Chapter01] 인터페이스 기능 확장

---
+++ src/main/java/chapter1/Ex12_UserDaoConnectionCountingTest.java
@@ -0,0 +1,20 @@
+package chapter1;
+
+import chapter1.connection.CountingConnectionMaker;
+import chapter1.dao.Ex6_RefactoringInterfaceUserDao;
+import chapter1.dao.factory.Ex12_CountingDaoFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Ex12_UserDaoConnectionCountingTest {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Ex12_CountingDaoFactory.class);
+        Ex6_RefactoringInterfaceUserDao dao = context.getBean("userDao", Ex6_RefactoringInterfaceUserDao.class);
+
+        // DAO 사용 코드
+
+        // DL(의존관계 검색)을 사용하여 빈을 가져옴
+        CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
+        System.out.println("Connection counter: " + ccm.getCounter());
+    }
+}
---
+++ src/main/java/chapter1/dao/factory/Ex12_CountingDaoFactory.java
@@ -0,0 +1,28 @@
+package chapter1.dao.factory;
+
+import chapter1.connection.ConnectionMaker;
+import chapter1.connection.CountingConnectionMaker;
+import chapter1.connection.DConnectionMaker;
+import chapter1.dao.Ex6_RefactoringInterfaceUserDao;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Ex12_CountingDaoFactory {
+
+    @Bean
+    public Ex6_RefactoringInterfaceUserDao userDao() {
+        return new Ex6_RefactoringInterfaceUserDao(connectionMaker());
+    }
+
+    @Bean
+    private ConnectionMaker connectionMaker() {
+        return new CountingConnectionMaker(realConnectionMaker());
+    }
+
+    @Bean
+    public ConnectionMaker realConnectionMaker() {
+        return new DConnectionMaker();
+    }
+
+}
