387c3667ebed96386b6f3a8a9904aa2d8936ad77
leeha0 <gkdud2837@gmail.com>
2020-11-10 02:01:04 +0900

    [Chapter02] 테스트 성공/실패 검증 코드 추가

--- src/main/java/chapter2/UserDaoTest.java
+++ src/main/java/chapter2/UserDaoTest.java
@@ -1,8 +1,15 @@
 package chapter2;
 
+import chapter1.domain.User;
+import chapter2.dao.UserDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.sql.SQLException;
+
 public class UserDaoTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 
         UserDao dao = context.getBean("userDao", UserDao.class);
@@ -17,9 +24,13 @@ public class UserDaoTest {
         System.out.println(user.getId() + " 등록 성공");
 
         User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
 
-        System.out.println(user2.getId() + " 조회 성공");
+        if (!user.getName().equals(user2.getName())) {
+            System.out.println("테스트 실패 (name)");
+        } else if (!user.getPassword().equals(user2.getPassword())) {
+            System.out.println("테스트 실패 (password)");
+        } else {
+            System.out.println("조회 테스트 성공");
+        }
     }
 }
