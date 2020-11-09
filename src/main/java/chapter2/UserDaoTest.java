dbf42f00c50e713e380c0a85ed91c3decb9e94a3
leeha0 <gkdud2837@gmail.com>
2020-11-09 19:35:47 +0900

    [Chapter01] 초난감 DAO

---
+++ src/main/java/chapter2/UserDaoTest.java
@@ -0,0 +1,25 @@
+package chapter2;
+
+public class UserDaoTest {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user = new User();
+        user.setId("user");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+}
