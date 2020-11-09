c5ab9f3f3d61355d4912c07ffc32f023ed6e0251
leeha0 <gkdud2837@gmail.com>
2020-11-09 20:11:10 +0900

    [Chapter01] 팩토리 중복 코드 제거

--- src/main/java/chapter1/dao/Ex7_DaoFactory.java
+++ src/main/java/chapter1/dao/Ex7_DaoFactory.java
@@ -1,18 +1,23 @@
 package chapter1.dao;
 
+import chapter1.connection.ConnectionMaker;
 import chapter1.connection.DConnectionMaker;
 
 public class Ex7_DaoFactory {
 
     public Ex6_RefactoringInterfaceUserDao userDao() {
-        return new Ex6_RefactoringInterfaceUserDao(new DConnectionMaker());
+        return new Ex6_RefactoringInterfaceUserDao(connectionMaker());
     }
 
 //    public AccountDao account() {
-//        return new AccountDao(new DConnectionMaker());
+//        return new AccountDao(connectionMaker());
 //    }
 //
 //    public MessageDao messageDao() {
-//        return new MessageDao(new DConnectionMaker());
+//        return new MessageDao(connectionMaker());
 //    }
+
+    private ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
+    }
 }
