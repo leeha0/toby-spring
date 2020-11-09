f19067b0316bb1666cd8293dcbf13d4ef403da5b
leeha0 <gkdud2837@gmail.com>
2020-11-09 19:44:10 +0900

    [Chapter01] 메서드 추출

---
+++ src/main/java/chapter1/dao/UserDao_ExtractMethod.java
@@ -0,0 +1,50 @@
+package chapter1.dao;
+
+import chapter1.domain.User;
+
+import java.sql.*;
+
+public class UserDao_ExtractMethod {
+
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Connection c = getConnection();
+
+        PreparedStatement ps = c.prepareStatement(
+                "insert into users(id, name, password) values (?, ?, ?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Connection c = getConnection();
+
+        PreparedStatement ps = c.prepareStatement(
+                "select * from users where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+
+    // 메서드 추출을 통해 중복 제거
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book");
+    }
+}
