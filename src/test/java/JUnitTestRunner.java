d474bb94394fc9221f1fe7bfa22c73961541c7e8
leeha0 <gkdud2837@gmail.com>
2020-11-11 01:23:56 +0900

    [Chapter02] JUnitRunner 추가

---
+++ src/test/java/JUnitTestRunner.java
@@ -0,0 +1,8 @@
+import org.junit.runner.JUnitCore;
+
+public class JUnitTestRunner {
+
+    public static void main(String[] args) {
+        JUnitCore.main("chapter2.UserDaoTest");
+    }
+}
--- src/test/java/chapter2/UserDaoTest.java
+++ src/test/java/chapter2/UserDaoTest.java
@@ -1,16 +1,33 @@
 package chapter2;
 
+import chapter1.domain.User;
 import chapter2.dao.UserDao;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.sql.SQLException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 public class UserDaoTest {
 
     @Test
-    public void addAndGet() {
+    public void addAndGet() throws SQLException, ClassNotFoundException {
         ApplicationContext context = new ClassPathXmlApplicationContext("chapter2/applicationContext.xml");
 
         UserDao dao = context.getBean("userDao", UserDao.class);
+        User user = new User();
+        user.setId("gyumee");
+        user.setName("박성철");
+        user.setPassword("springno1");
+
+        dao.add(user);
+
+        User user2 = dao.get(user.getId());
+
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
     }
 }
